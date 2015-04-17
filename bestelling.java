5dc981ae6b2c2f466187c1683fdb7f1b3e8751eb
wfikkert <wfikkert@gmail.com>
2015-04-17 20:21:12 +0200

    Create bestelling.java

---
+++ bestelling.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+
+
+
+
+public class Bestelling {
+    private int ordernr;
+    private Date datum;
+    private int artikelnr;
+
+    public Bestelling(int ordernr, Date datum, int artikelnr) {
+        this.ordernr = ordernr;
+        this.datum = datum;
+        this.artikelnr = artikelnr;
+    }
+
+    public int getOrdernr() {
+        return ordernr;
+    }
+
+    public Date getDatum() {
+        return datum;
+    }
+
+    public int getArtikelnr() {
+        return artikelnr;
+    
+  
+    }
+}
